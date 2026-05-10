0776fbcba1d5e8d4e942e5b74f65a01edce39e5d
Šimon Brauner <simonbrauner@seznam.cz>
2026-05-10 14:23:28 +0200

    Display images from blob storage

--- web-app/app.py
+++ web-app/app.py
@@ -1,11 +1,26 @@
 from flask import Flask, render_template
 
+from azure.storage.blob import BlobServiceClient
+from azure.identity import DefaultAzureCredential
+
+BLOB_ENDPOINT = "https://colorqueue.blob.core.windows.net"
+CONTAINER_NAME = "images"
+MAX_IMAGE_COUNT = 9
+
 app = Flask(__name__)
+
+credential = DefaultAzureCredential()
+
+blob_client = BlobServiceClient(account_url=BLOB_ENDPOINT, credential=credential)
 
 @app.route("/")
 def index():
     image_paths = []
-    for _ in range(9):
-        image_paths.append("/static/a.png")
+
+    container_client = blob_client.get_container_client(CONTAINER_NAME)
+    images = list(container_client.list_blobs())
+    images.sort(key=lambda x: x.last_modified, reverse=True)
+    for image in images[:MAX_IMAGE_COUNT]:
+        image_paths.append(f"{BLOB_ENDPOINT}/{CONTAINER_NAME}/{image.name}")
 
     return render_template("index.html", image_paths=image_paths)
